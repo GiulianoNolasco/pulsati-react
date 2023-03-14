6d30fd5e21ec069d7e864bae2e0f6da6f75386fc
Giuliano Nolasco <110136470+GiulianoNolasco@users.noreply.github.com>
2023-03-14 14:27:57 -0300

    commit

--- passageiros/src/pages-voos/voosAtualizar.tsx
+++ passageiros/src/pages-voos/voosAtualizar.tsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Aeroporto } from "../model/Aeroporto";
+import { buscarAeroportos } from "../services/aeroportos";
 import { getVoo, putVoo } from "../services/voos";
 
 import "./voosAtualizar.css";
 
 export function VoosAtualizar() {
   const [voo, setvoo] = useState<any>();
+  const [aeroportos, setCargos] = useState<Aeroporto[]>([]);
 
   const params = useParams();
-
   if (!voo) {
     getVoo(parseInt(params.id || "", 10)).then((res) => setvoo(res.data));
   }
@@ -17,47 +19,96 @@ export function VoosAtualizar() {
     evt.preventDefault();
     const formData = new FormData(evt.target);
     const data = Object.fromEntries(formData);
-    // putVoo({
-    //   codigo: voo.codigo,
-    //   nome: data.nome.toString(),
-    //   capacidade: parseInt(data.capacidade.toString(), 10),
-    // });
+    putVoo({
+      codigo: parseInt(voo.codigo),
+      codigoVoo: parseInt(data.codigoVoo.toString()),
+
+      codigoAeroportoOrigem: parseInt(
+        data.codigo_codigoAeroportoOrigem.toString(),
+        10
+      ),
+      codigoAeroportoDestino: parseInt(
+        data.codigo_codigoAeroportoDestino.toString(),
+        10
+      ),
+      nomeEmpresaAerea: data.nomeEmpresaAerea.toString(),
+    });
     alert(
-      `Voo Atualizado: Nome: ${data.nome.toString()}, Capacidade: ${parseInt(
-        data.capacidade.toString()
-      )}, codigo ${voo.codigo}`
+      `Voo Alterado: \n
+      codigoVoo: ${parseInt(data.codigoVoo.toString())}, \n
+      codigoAeroportoOrigem: ${parseInt(
+        data.codigo_codigoAeroportoOrigem.toString(),
+        10
+      )},\n
+      codigoAeroportoDestino: ${parseInt(
+        data.codigo_codigoAeroportoDestino.toString(),
+        10
+      )},\n
+        nomeEmpresaAerea: ${data.nomeEmpresaAerea.toString()}
+        
+        `
     );
   };
+
+  const loadCargos = async () => {
+    const aeroportos = await buscarAeroportos();
+    setCargos(aeroportos);
+  };
+
+  useEffect(() => {
+    loadCargos();
+  }, []);
 
   return (
     <div className="divtabela">
       <form onSubmit={onSubmit}>
         <div className="linha">
-          <span>Nome:</span>
+          <span>Código Voo</span>
           <input
-            type="text"
-            name="nome"
-            defaultValue={voo?.nome}
+            type="number"
+            name="codigoVoo"
             className="inputnormal"
+            defaultValue={voo?.codigoVoo}
           />
         </div>
 
         <div className="linha">
-          <span>Capacidade:</span>
+          <span>Nome Emp Aérea</span>
           <input
-            type="number"
-            name="capacidade"
-            defaultValue={voo?.capacidade}
+            type="text"
+            name="nomeEmpresaAerea"
             className="inputnormal"
+            defaultValue={voo?.nomeEmpresaAerea}
           />
         </div>
+
         <div className="linha">
-          <input type="submit" value="Atualizar" className="submitInput" />
+          <span>Cd Aer Origem</span>
+          <select name="codigo_codigoAeroportoOrigem" className="inputnormal">
+            {aeroportos.map((aeroporto) => {
+              return <option value={aeroporto.codigo}>{aeroporto.nome}</option>;
+            })}
+          </select>
         </div>
+
+        <div className="linha">
+          <span>Cd Aer Destino</span>
+          <select name="codigo_codigoAeroportoDestino" className="inputnormal">
+            {aeroportos.map((aeroporto) => {
+              return (
+                <option
+                  defaultValue={aeroporto.codigo}
+                  value={aeroporto.codigo}
+                >
+                  {aeroporto.nome}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+
+        <input type="submit" value="Salvar" className="inputsalvar" />
       </form>
-      <a href="/voos">
-        <button>Voltar</button>
-      </a>
     </div>
   );
 }
--- passageiros/src/pages-voos/voosVer.tsx
+++ passageiros/src/pages-voos/voosVer.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Passageiro } from "../model/Passageiro";
+import { buscarPassageiros, deletePassageiro } from "../services/passageiros";
 import { getVoo } from "../services/voos";
 import "./voosver.css";
 
@@ -13,25 +15,94 @@ export function VoosVer() {
     getVoo(parseInt(params.id || "", 10)).then((res) => setvoo(res.data));
   }
 
+  const [passageiros, setpassageiros] = useState<any[]>();
+
+  const loadData = async () => {
+    const passageiros: Passageiro[] = await buscarPassageiros();
+    setpassageiros(passageiros);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const onClickDeletePassageiro = (id: number) => {
+    deletePassageiro(id).then(() => loadData());
+  };
   return (
     <div className="divtabela">
       <div>
-        <a className="tituloSpan">Voo:</a>
-        <div className="linha">
-          <span>Código:</span>
-          <input type="text" value={voo?.codigo} className="inputnormal2" />
+        <div>
+          <a className="tituloSpan">Voo:</a>
+          <div className="linha">
+            <span>ID:</span>
+            <input type="text" value={voo?.codigo} className="inputnormal2" />
+          </div>
+          <div className="linha">
+            <span>Código Voo:</span>
+            <input
+              type="text"
+              value={voo?.codigoVoo}
+              className="inputnormal2"
+            />
+          </div>
+          <div className="linha">
+            <span>Código Aeroporto Origem:</span>
+            <input
+              type="text"
+              value={voo?.codigoAeroportoOrigem}
+              className="inputnormal2"
+            />
+          </div>
+          <div className="linha">
+            <span>Código Aeroporto Destino:</span>
+            <input
+              type="text"
+              value={voo?.codigoAeroportoDestino}
+              className="inputnormal2"
+            />
+          </div>
+          <div className="linha">
+            <span>Nome Empresa Aérea:</span>
+            <input
+              type="text"
+              value={voo?.nomeEmpresaAerea}
+              className="inputnormal2"
+            />
+          </div>
+          <a href="/voos">
+            <button>Voltar</button>
+          </a>
         </div>
-        <div className="linha">
-          <span>Nome:</span>
-          <input type="text" value={voo?.nome} className="inputnormal2" />
-        </div>
-        <div className="linha">
-          <span>Capacidade:</span>
-          <input type="text" value={voo?.capacidade} className="inputnormal2" />
-        </div>
-        <a href="/voos">
-          <button>Voltar</button>
-        </a>
+      </div>
+      <div>
+        <span className="tituloSpan">Passageiros do Voo</span>
+
+        <table className="tabela">
+          <thead>
+            <td>Id</td>
+            <td>Nome</td>
+            <td>Codigo Vôo</td>
+          </thead>
+          <tbody>
+            {passageiros?.map((passageiro) => {
+              if (passageiro.codigoVoo == voo.codigoVoo) {
+                return (
+                  <tr>
+                    <td>{passageiro.codigo}</td>
+                    <td>{passageiro.nome}</td>
+                    <td>{passageiro.codigoVoo}</td>
+                    <td>
+                      <a href={`/passageiros/${passageiro.codigo}`}>
+                        Visualizar
+                      </a>
+                    </td>
+                  </tr>
+                );
+              }
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
